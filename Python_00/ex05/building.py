883fef951f8e6165343beb58f244f22916e742a3
Isabel Tovar <itovarnuez@gmail.com>
2025-04-16 11:46:15 +0200

    Flake8

--- Python_00/ex05/building.py
+++ Python_00/ex05/building.py
@@ -12,23 +12,28 @@ This script counts various properties of the input text, including:
 Usage:
     Run the script with an optional argument:
     $ python script_name.py "Your text here"
-    
+
     If no argument is provided, you will be prompted to enter text.
 
 Note:
     - The script handles multiple lines of text if input through the prompt.
-    - It considers '...' as a special punctuation case and counts it accordingly.
+    - It considers '...' as a special punctuation case and counts as it.
 
 Author: Isabel Tovar
 Date: 10 April 2025
 """
 
 import sys
+
+
 def count_print(text, carriage_return):
 
-    punctuation_marks = ['.', '?', '!', ',', ':',';', '-','—', '[', ']', '{', '}', '(', ')', "'", '"']
+    punctuation_marks = [
+        '.', '?', '!', ',', ':', ';', '-',
+        '—', '[', ']', '{', '}', '(', ')', "'", '"']
 
-    count_marks = sum(text.count(mark) for mark in punctuation_marks) - 2 * text.count('...')
+    count_marks = sum(text.count(mark)
+                      for mark in punctuation_marks) - 2 * text.count('...')
     count_upper = sum(1 for char in text if char.isupper())
     count_lower = sum(1 for char in text if char.islower())
     count_space = text.count(' ') + carriage_return
@@ -44,11 +49,11 @@ def count_print(text, carriage_return):
 
 def main():
 
-    carriage_return  = 0
+    carriage_return = 0
 
     if len(sys.argv) <= 2:
         if len(sys.argv) == 1:
-            print ("What is the text to count?")
+            print("What is the text to count?")
             text = ""
             while text == "":
                 carriage_return += 1
--- Python_00/ex05/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-
-# your tests and your error handling
-
-if __name__ == "__main__":
-    main()
